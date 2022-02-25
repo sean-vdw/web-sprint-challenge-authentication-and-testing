717f5fb7e2cdbdef9cc291c0bf1b225b02ddd097
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-25 08:27:23 -0800

    set up server with express sessions

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const restrict = require('./middleware/restricted.js');
 
@@ -12,6 +13,18 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session({
+  name: 'dadjokes',
+  secret: 'why did the chicken cross the road',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 3,
+    secure: false,
+    httpOnly: false
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: false
+}));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
