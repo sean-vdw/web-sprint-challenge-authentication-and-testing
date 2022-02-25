53e9321fec0be49802cfc4c814debab5ef103c73
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-25 13:50:04 -0800

    finished tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,51 @@
-// Write your tests here
+const server = require('./server');
+const db = require('../data/dbConfig');
+const request = require('supertest');
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+// Sanity Check
 test('sanity', () => {
-  expect(true).toBe(false)
-})
+  expect(true).toBe(true);
+});
+
+// Endpoint Tests
+describe('[GET] /jokes', () => {
+  it('should return token required if attempted to access', async () => {
+    const res = await request(server).get('/api/jokes');
+    expect(res.body.message).toBe('token required');
+  });
+  it('should return a status of 401', async () => {
+    const res = await request(server).get('/api/jokes');
+    expect(res.status).toBe(401);
+  });
+});
+
+describe('[POST] /register', () => {
+  it('should return object of new user added', async () => {
+    const res = await request(server).post('/api/auth/register').send({ username: 'Sean', password: '1234' });
+    expect(res.body).toBeTruthy();
+  });
+  it('should return a status of 201', async () => {
+    const res = await request(server).post('/api/auth/register').send({ username: 'Kyle', password: '1234' });
+    expect(res.status).toBe(201);
+  });
+});
+
+describe('[POST] /login', () => {
+  it('should return message of invalid credentials if user does not exist', async () => {
+    const res = await request(server).post('/api/auth/login').send({ username: 'Jolene', password: '1234' });
+    expect(res.body.message).toBe('invalid credentials');
+  });
+  it('should return a status of 401 with invalid credentials', async () => {
+    const res = await request(server).post('/api/auth/login').send({ username: 'Jolene', password: '1234' });
+    expect(res.status).toBe(401);
+  });
+});
